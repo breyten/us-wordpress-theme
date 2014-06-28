57a7851456feb56c49ddeede3c22d947cfcc19e0
Breyten Ernsting <bje@dds.nl>
2014-06-28 17:05:04 +0200

    Wroking navigation menu.

--- header.php
+++ header.php
@@ -40,67 +40,35 @@
 	</button><a class="navbar-brand" href="/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-top.png" alt="US Volleybal Amsterdam" /></a>
       </div>
       <div class="collapse navbar-collapse">
-		<?php wp_nav_menu(array(
+		<?php
+        // ugly hack to get bootstrap compatible menus :/
+        $menu = wp_nav_menu(array(
+            'echo' => false,
 			'theme_location' => 'primary',
 			'container' => false,
-			'menu_class' => 'nav navbar-nav navbar-right',
-			'items_wrap' => '<ol id="%1$s" class="%2$s">%3$s</ol>',
-			'walker' => new BootstrapBasicMyWalkerNavMenu()
-		)); ?>
+		));
+        echo str_ireplace(
+            array(
+                '<ul>',
+                '<div class="menu">',
+                '</ul></div>',
+                'class="page_item ',
+                '<ul class=\'children\'>',
+                'page_item_has_children'
+            ),
+            array(
+                '<ul class="nav navbar-nav navbar-right">',
+                '',
+                '</ul>',
+                'class="',
+                '<ul class="dropdown-menu">',
+                'dropdown'
+            ),
+            $menu
+        );
+        ?>
 		<?php dynamic_sidebar('navbar-right'); ?>
       </div>
     </div>
   </div><!--Begin content-->
   <div id="content">
-
-<!--
-		<div class="container page-container">
-			<?php do_action('before'); ?>
-			<header role="banner">
-				<div class="row row-with-vspace site-branding">
-					<div class="col-md-6 site-title">
-						<h1 class="site-title-heading">
-							<a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-						</h1>
-						<div class="site-description">
-							<small>
-								<?php bloginfo('description'); ?>
-							</small>
-						</div>
-					</div>
-					<div class="col-md-6 page-header-top-right">
-						<div class="sr-only">
-							<a href="#content" title="<?php esc_attr_e('Skip to content', 'bootstrap-basic'); ?>"><?php _e('Skip to content', 'bootstrap-basic'); ?></a>
-						</div>
-						<?php if (is_active_sidebar('header-right')) { ?>
-						<div class="pull-right">
-							<?php dynamic_sidebar('header-right'); ?>
-						</div>
-						<div class="clearfix"></div>
-						<?php } // endif; ?>
-					</div>
-				</div><!--.site-branding-->
-<!--
-				<div class="row main-navigation">
-					<div class="col-md-12">
-						<nav class="navbar navbar-default" role="navigation">
-							<div class="navbar-header">
-								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-primary-collapse">
-									<span class="sr-only"><?php _e('Toggle navigation', 'bootstrap-basic'); ?></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</button>
-							</div>
-
-							<div class="collapse navbar-collapse navbar-primary-collapse">
-								<?php wp_nav_menu(array('theme_location' => 'primary', 'container' => false, 'menu_class' => 'nav navbar-nav', 'walker' => new BootstrapBasicMyWalkerNavMenu())); ?>
-								<?php dynamic_sidebar('navbar-right'); ?>
-							</div><!--.navbar-collapse--><!--
-						</nav>
-					</div>
-				</div><!--.main-navigation--><!--
-			</header>
-
-
-			<div id="content" class="row row-with-vspace site-content"> -->
--- inc/BootstrapBasicMyWalkerNavMenu.php
+++ inc/BootstrapBasicMyWalkerNavMenu.php
@@ -1,120 +1,125 @@
 <?php
-/**
- * My walker nav menu extends wp walker nav menu
- * 
- * @package bootstrap-basic
- */
+
+class Bootstrap_Walker_Nav_Menu extends Walker_Nav_Menu {
 
 
-class BootstrapBasicMyWalkerNavMenu extends Walker_Nav_Menu
-{
-	
-	
-	//Overwrite display_element function to add has_children attribute. Not needed in >= Wordpress 3.4
-	/**
-	 * @link https://gist.github.com/duanecilliers/1817371 copy from this url
-	 */
-	function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output)
-	{
-		if (!$element)
-			return;
-		$id_field = $this->db_fields['id'];
+	function start_lvl( &$output, $depth ) {
 
-		//display this element
-		if (is_array($args[0]))
-			$args[0]['has_children'] = !empty($children_elements[$element->$id_field]);
-		else if (is_object($args[0]))
-			$args[0]->has_children = !empty($children_elements[$element->$id_field]);
-		$cb_args = array_merge(array(&$output, $element, $depth), $args);
-		call_user_func_array(array(&$this, 'start_el'), $cb_args);
+		//In a child UL, add the 'dropdown-menu' class
+		$indent = str_repeat( "\t", $depth );
+		$output .= "\n$indent<ul class=\"dropdown-menu\">\n";
 
-		$id = $element->$id_field;
-
-		// descend only when the depth is right and there are childrens for this element
-		if (($max_depth == 0 || $max_depth > $depth + 1) && isset($children_elements[$id])) {
-
-			foreach ($children_elements[$id] as $child) {
-
-				if (!isset($newlevel)) {
-					$newlevel = true;
-					//start the child delimiter
-					$cb_args = array_merge(array(&$output, $depth), $args);
-					call_user_func_array(array(&$this, 'start_lvl'), $cb_args);
-				}
-				$this->display_element($child, $children_elements, $max_depth, $depth + 1, $args, $output);
-			}
-			unset($children_elements[$id]);
-		}
-
-		if (isset($newlevel) && $newlevel) {
-			//end the child delimiter
-			$cb_args = array_merge(array(&$output, $depth), $args);
-			call_user_func_array(array(&$this, 'end_lvl'), $cb_args);
-		}
-
-		//end this element
-		$cb_args = array_merge(array(&$output, $element, $depth), $args);
-		call_user_func_array(array(&$this, 'end_el'), $cb_args);
-	}// display_element
-	
-	
-	/**
-	 * @link https://gist.github.com/duanecilliers/1817371 copy from this url
-	 */
-	public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) 
-	{
-		if ((is_object($item) && $item->title == null) || (!is_object($item))) {
-			return ;
-		}
-		
-		$indent = ($depth) ? str_repeat("\t", $depth) : '';
-
-		$li_attributes = '';
-		$class_names = $value = '';
-
-		$classes = empty($item->classes) ? array() : (array) $item->classes;
-		//Add class and attribute to LI element that contains a submenu UL.
-		if (is_object($args) && $args->has_children) {
-			//$classes[] = 'dropdown';
-			$li_attributes .= ' data-dropdown="dropdown"';
-		}
-		$classes[] = 'menu-item-' . $item->ID;
-		//If we are on the current page, add the active class to that menu item.
-		$classes[] = ($item->current) ? 'active' : '';
-
-		//Make sure you still add all of the WordPress classes.
-		$class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
-		$class_names = ' class="' . esc_attr($class_names) . '"';
-
-		$id = apply_filters('nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args);
-		$id = strlen($id) ? ' id="' . esc_attr($id) . '"' : '';
-
-		$output .= $indent . '<li' . $id . $value . $class_names . $li_attributes . '>';
-
-		//Add attributes to link element.
-		$attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
-		$attributes .=!empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
-		$attributes .=!empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
-		$attributes .=!empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
-		$attributes .= (is_object($args) && $args->has_children) ? ' class="dropdown-toggle" data-toggle="dropdown"' : '';
-
-		$item_output = (is_object($args)) ? $args->before : '';
-		$item_output .= '<a' . $attributes . '>';
-		$item_output .= (is_object($args) ? $args->link_before : '') . apply_filters('the_title', $item->title, $item->ID) . (is_object($args) ? $args->link_after : '');
-		$item_output .= (is_object($args) && $args->has_children) ? ' <span class="caret"></span> ' : '';
-		$item_output .= '</a>';
-		$item_output .= (is_object($args) ? $args->after : '');
-
-		$output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
-	}// start_el
-
-
-	public function start_lvl(&$output, $depth = 0, $args = array()) 
-	{
-		$indent = str_repeat("\t", $depth);
-		$output .= "\n$indent<ul class=\"sub-menu dropdown-menu\">\n";
 	}
 
+	/**
+	* Start the element output.
+	*
+	* @see Walker::start_el()
+	*
+	* @since 3.0.0
+	*
+	* @param string $output Passed by reference. Used to append additional content.
+	* @param object $item   Menu item data object.
+	* @param int    $depth  Depth of menu item. Used for padding.
+	* @param array  $args   An array of arguments. @see wp_nav_menu()
+	* @param int    $id     Current item ID.
+	*/
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		$class_names = '';
+
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+
+		/**
+		* Filter the CSS class(es) applied to a menu item's <li>.
+		*
+		* @since 3.0.0
+		*
+		* @see wp_nav_menu()
+		*
+		* @param array  $classes The CSS classes that are applied to the menu item's <li>.
+		* @param object $item    The current menu item.
+		* @param array  $args    An array of wp_nav_menu() arguments.
+		*/
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+		/**
+		* Filter the ID applied to a menu item's <li>.
+		*
+		* @since 3.0.1
+		*
+		* @see wp_nav_menu()
+		*
+		* @param string $menu_id The ID that is applied to the menu item's <li>.
+		* @param object $item    The current menu item.
+		* @param array  $args    An array of wp_nav_menu() arguments.
+		*/
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$output .= $indent . '<li' . $id . $class_names .'>';
+
+		$atts = array();
+		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+		$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+		$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+		$atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+		/**
+		* Filter the HTML attributes applied to a menu item's <a>.
+		*
+		* @since 3.6.0
+		*
+		* @see wp_nav_menu()
+		*
+		* @param array $atts {
+		*     The HTML attributes applied to the menu item's <a>, empty strings are ignored.
+		*
+		*     @type string $title  Title attribute.
+		*     @type string $target Target attribute.
+		*     @type string $rel    The rel attribute.
+		*     @type string $href   The href attribute.
+		* }
+		* @param object $item The current menu item.
+		* @param array  $args An array of wp_nav_menu() arguments.
+		*/
+		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( ! empty( $value ) ) {
+				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		/** This filter is documented in wp-includes/post-template.php */
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+
+		/**
+		* Filter a menu item's starting output.
+		*
+		* The menu item's starting output only includes $args->before, the opening <a>,
+		* the menu item's title, the closing </a>, and $args->after. Currently, there is
+		* no filter for modifying the opening and closing <li> for a menu item.
+		*
+		* @since 3.0.0
+		*
+		* @see wp_nav_menu()
+		*
+		* @param string $item_output The menu item's starting HTML output.
+		* @param object $item        Menu item data object.
+		* @param int    $depth       Depth of menu item. Used for padding.
+		* @param array  $args        An array of wp_nav_menu() arguments.
+		*/
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
 
 }
-
