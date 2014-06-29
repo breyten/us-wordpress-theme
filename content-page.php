750a13cee3e2e741127e0daf9b0574a1a1360229
Breyten Ernsting <bje@dds.nl>
2014-06-29 14:04:50 +0200

    More sensible breadcrumbs in pages.

--- content-page.php
+++ content-page.php
@@ -4,7 +4,14 @@
       <li>
         <a href="/"><span class="glyphicon glyphicon-home"></span></a>
       </li>
-      <li>
+
+      <?php if($post->post_parent) {
+      	$parent_link = get_permalink($post->post_parent);
+      	$parent_title = get_the_title($post->post_parent);
+      ?>
+      <li><a href="<?php echo $parent_link; ?>"><?php echo $parent_title; ?></a></li>
+      <?php  }
+      ?>      <li>
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
       </li>
     </ul>
