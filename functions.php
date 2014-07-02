e967a7e194aa69f44130e2fb10897419933ca9c9
Breyten Ernsting <bje@dds.nl>
2014-07-02 21:35:27 +0200

    Remove image size attributes and style header a bit.

--- functions.php
+++ functions.php
@@ -216,6 +216,50 @@ function my_rewrite_flush() {
 add_action( 'after_switch_theme', 'my_rewrite_flush' );
 
 /**
+ * This is a modification of image_downsize() function in wp-includes/media.php
+ * we will remove all the width and height references, therefore the img tag
+ * will not add width and height attributes to the image sent to the editor.
+ *
+ * @param bool false No height and width references.
+ * @param int $id Attachment ID for image.
+ * @param array|string $size Optional, default is 'medium'. Size of image, either array or string.
+ * @return bool|array False on failure, array on success.
+ */
+function myprefix_image_downsize( $value = false, $id, $size ) {
+    if ( !wp_attachment_is_image($id) )
+        return false;
+
+    $img_url = wp_get_attachment_url($id);
+    $is_intermediate = false;
+    $img_url_basename = wp_basename($img_url);
+
+    // try for a new style intermediate size
+    if ( $intermediate = image_get_intermediate_size($id, $size) ) {
+        $img_url = str_replace($img_url_basename, $intermediate['file'], $img_url);
+        $is_intermediate = true;
+    }
+    elseif ( $size == 'thumbnail' ) {
+        // Fall back to the old thumbnail
+        if ( ($thumb_file = wp_get_attachment_thumb_file($id)) && $info = getimagesize($thumb_file) ) {
+            $img_url = str_replace($img_url_basename, wp_basename($thumb_file), $img_url);
+            $is_intermediate = true;
+        }
+    }
+
+    // We have the actual image size, but might need to further constrain it if content_width is narrower
+    if ( $img_url) {
+        return array( $img_url, 0, 0, $is_intermediate );
+    }
+    return false;
+}
+
+/* Remove the height and width refernces from the image_downsize function.
+ * We have added a new param, so the priority is 1, as always, and the new
+ * params are 3.
+ */
+add_filter( 'image_downsize', 'myprefix_image_downsize', 1, 3 );
+
+/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
