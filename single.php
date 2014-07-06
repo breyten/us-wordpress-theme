10851d5de1c78602d2aae8a83e0466e85ba8446c
Breyten Ernsting <bje@dds.nl>
2014-07-06 15:48:50 +0200

    Only add the header class if the post had a featured image.

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Template for dispalying single post (read full post page).
- * 
+ *
  * @package bootstrap-basic
  */
 
@@ -14,7 +14,7 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 
 // check if the post has a Post Thumbnail assigned to it.
 if ((get_post_type() == 'team') && has_post_thumbnail() ) {
-?> 
+?>
 <section id="header">
   	<?php the_post_thumbnail('large'); ?>
 </section>
@@ -22,33 +22,40 @@ if ((get_post_type() == 'team') && has_post_thumbnail() ) {
 <?php get_sidebar('left'); ?>
 
 <!-- Needs check if header image exist, else: .no-header -->
-<section id="main" class="no-header">
+<?php
+if (has_post_thumbnail()) {
+    $main_class='class="no-header"';
+} else {
+    $main_class="";
+}
+?>
+<section id="main" <?php echo $main_class; ?>>
   <div class="container white">
     <div class="row">
       <?php
     if (get_post_type() == 'team') {
-		  $table_width = 8;           
+		  $table_width = 8;
     } else {
 		  $table_width = 9;
-    }      
+    }
       ?>
       <div id="nieuws" class="col-sm-8 col-md-<?php echo $table_width; ?>">
-				<?php 
+				<?php
 				while (have_posts()) {
 					the_post();
 
           if (get_post_type() == 'team') {
-					  get_template_part('content', 'team');            
+					  get_template_part('content', 'team');
           } else {
 					  get_template_part('content', get_post_format());
           }
 
 					echo "\n\n";
-					
+
 					bootstrapBasicPagination();
 
 					echo "\n\n";
-					
+
 					// If comments are open or we have at least one comment, load up the comment template
 					if (comments_open() || '0' != get_comments_number()) {
 						comments_template();
@@ -57,18 +64,18 @@ if ((get_post_type() == 'team') && has_post_thumbnail() ) {
 					echo "\n\n";
 
 				} //endwhile;
-				?>         
+				?>
       </div>
       <!-- sidebar thingie -->
       <?php
       if (get_post_type() == 'team') {
-			  get_template_part('content-sidebar', 'team');            
+			  get_template_part('content-sidebar', 'team');
       } else {
 			  get_template_part('content-sidebar', 'recent');
       }
       ?>
     </div>
   </div>
-</section>            
-<?php //get_sidebar('right'); ?> 
-<?php get_footer(); ?> 
+</section>
+<?php //get_sidebar('right'); ?>
+<?php get_footer(); ?>
