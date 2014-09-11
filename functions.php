1e8d072ce2af4495d8ba82b81be244aedbb38730
Tom Rutgers <tom@steam.nl>
2014-09-11 23:50:09 +0200

    Fix .no-header class since featured image is always present for news items Remove min.'s from head, functions already get called from footer (fix responsive sidebar menu from slideToggle'ing twice)

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
  * Required WordPress variable.
  */
 if (!isset($content_width)) {
-	$content_width = 1170;
+  $content_width = 1170;
 }
 
 
@@ -19,39 +19,39 @@ if (!isset($content_width)) {
  */
 function bootstrapBasicSetup()
 {
-	/**
-	 * Make theme available for translation
-	 * Translations can be filed in the /languages/ directory
-	 *
-	 * copy from underscores theme
-	 */
-	load_theme_textdomain('bootstrap-basic', get_template_directory() . '/languages');
+  /**
+   * Make theme available for translation
+   * Translations can be filed in the /languages/ directory
+   *
+   * copy from underscores theme
+   */
+  load_theme_textdomain('bootstrap-basic', get_template_directory() . '/languages');
 
-	// add theme support post and comment automatic feed links
-	add_theme_support('automatic-feed-links');
+  // add theme support post and comment automatic feed links
+  add_theme_support('automatic-feed-links');
 
-	// enable support for post thumbnail or feature image on posts and pages
-	add_theme_support('post-thumbnails');
+  // enable support for post thumbnail or feature image on posts and pages
+  add_theme_support('post-thumbnails');
 
-	// add support menu
-	register_nav_menus(array(
-		'primary' => __('Primary Menu', 'bootstrap-basic'),
-	));
+  // add support menu
+  register_nav_menus(array(
+    'primary' => __('Primary Menu', 'bootstrap-basic'),
+  ));
 
-	// add post formats support
-	add_theme_support('post-formats', array('aside', 'image', 'video', 'quote', 'link'));
+  // add post formats support
+  add_theme_support('post-formats', array('aside', 'image', 'video', 'quote', 'link'));
 
-	// add support custom background
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'bootstrap_basic_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => ''
-			)
-		)
-	);
+  // add support custom background
+  add_theme_support(
+    'custom-background',
+    apply_filters(
+      'bootstrap_basic_custom_background_args',
+      array(
+        'default-color' => 'ffffff',
+        'default-image' => ''
+      )
+    )
+  );
 }// bootstrapBasicSetup
 add_action('after_setup_theme', 'bootstrapBasicSetup');
 
@@ -61,59 +61,59 @@ add_action('after_setup_theme', 'bootstrapBasicSetup');
  */
 function bootstrapBasicWidgetsInit()
 {
-	register_sidebar(array(
-		'name'          => __('Header right', 'bootstrap-basic'),
-		'id'            => 'header-right',
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	));
+  register_sidebar(array(
+    'name'          => __('Header right', 'bootstrap-basic'),
+    'id'            => 'header-right',
+    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ));
 
-	register_sidebar(array(
-		'name'          => __('Navigation bar right', 'bootstrap-basic'),
-		'id'            => 'navbar-right',
-		'before_widget' => '',
-		'after_widget'  => '',
-		'before_title'  => '',
-		'after_title'   => '',
-	));
+  register_sidebar(array(
+    'name'          => __('Navigation bar right', 'bootstrap-basic'),
+    'id'            => 'navbar-right',
+    'before_widget' => '',
+    'after_widget'  => '',
+    'before_title'  => '',
+    'after_title'   => '',
+  ));
 
-	register_sidebar(array(
-		'name'          => __('Sidebar left', 'bootstrap-basic'),
-		'id'            => 'sidebar-left',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	));
+  register_sidebar(array(
+    'name'          => __('Sidebar left', 'bootstrap-basic'),
+    'id'            => 'sidebar-left',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ));
 
-	register_sidebar(array(
-		'name'          => __('Sidebar right', 'bootstrap-basic'),
-		'id'            => 'sidebar-right',
-		//'before_widget' => '<span id="%1$s">',
-		//'after_widget'  => '</span>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	));
+  register_sidebar(array(
+    'name'          => __('Sidebar right', 'bootstrap-basic'),
+    'id'            => 'sidebar-right',
+    //'before_widget' => '<span id="%1$s">',
+    //'after_widget'  => '</span>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ));
 
-	register_sidebar(array(
-		'name'          => __('Footer left', 'bootstrap-basic'),
-		'id'            => 'footer-left',
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	));
+  register_sidebar(array(
+    'name'          => __('Footer left', 'bootstrap-basic'),
+    'id'            => 'footer-left',
+    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ));
 
-	register_sidebar(array(
-		'name'          => __('Footer right', 'bootstrap-basic'),
-		'id'            => 'footer-right',
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	));
+  register_sidebar(array(
+    'name'          => __('Footer right', 'bootstrap-basic'),
+    'id'            => 'footer-right',
+    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ));
 }// bootstrapBasicWidgetsInit
 add_action('widgets_init', 'bootstrapBasicWidgetsInit');
 
@@ -123,18 +123,18 @@ add_action('widgets_init', 'bootstrapBasicWidgetsInit');
  */
 function bootstrapBasicEnqueueScripts()
 {
-	wp_enqueue_style('bootstrap-basic-style', get_stylesheet_uri());
-	wp_enqueue_style('bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css');
-	wp_enqueue_style('bootstrap-theme-style', get_template_directory_uri() . '/css/bootstrap-theme.min.css');
-	wp_enqueue_style('fontawesome-style', get_template_directory_uri() . '/css/font-awesome.min.css');
-	wp_enqueue_style('main-style', get_template_directory_uri() . '/css/main.css');
+  wp_enqueue_style('bootstrap-basic-style', get_stylesheet_uri());
+  wp_enqueue_style('bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css');
+  wp_enqueue_style('bootstrap-theme-style', get_template_directory_uri() . '/css/bootstrap-theme.min.css');
+  wp_enqueue_style('fontawesome-style', get_template_directory_uri() . '/css/font-awesome.min.css');
+  wp_enqueue_style('main-style', get_template_directory_uri() . '/css/main.css');
 
-	wp_enqueue_script('modernizr-script', get_template_directory_uri() . '/js/vendor/modernizr.min.js');
-	wp_enqueue_script('respond-script', get_template_directory_uri() . '/js/vendor/respond.min.js');
-	wp_enqueue_script('html5-shiv-script', get_template_directory_uri() . '/js/vendor/html5shiv.js');
-	wp_enqueue_script('jquery');
-	wp_enqueue_script('bootstrap-script', get_template_directory_uri() . '/js/vendor/bootstrap.min.js');
-	wp_enqueue_script('main-script', get_template_directory_uri() . '/js/main.js');
+  wp_enqueue_script('modernizr-script', get_template_directory_uri() . '/js/vendor/modernizr.min.js');
+  wp_enqueue_script('respond-script', get_template_directory_uri() . '/js/vendor/respond.min.js');
+  wp_enqueue_script('html5-shiv-script', get_template_directory_uri() . '/js/vendor/html5shiv.js');
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('bootstrap-script', get_template_directory_uri() . '/js/vendor/bootstrap.min.js');
+  
 }// bootstrapBasicEnqueueScripts
 add_action('wp_enqueue_scripts', 'bootstrapBasicEnqueueScripts');
 
@@ -166,44 +166,44 @@ add_action( 'admin_head', 'fb_move_admin_bar' );
 add_action( 'wp_head', 'fb_move_admin_bar' );
 
 function us_custom_init() {
-	register_post_type(
-		'sponsor',
-		array(
-			'label' => 'Sponsors',
-			'labels' => array(
-				'singular_name' => 'Sponsor',
-			),
-			'description' => 'Een sponsor',
-			'public' => true,
-			'exclude_from_search' => true,
-			'publicly_queryable' => false,
-			'show_in_nav_menus' => false,
-			'menu_position' => 8,
-		)
-	);
-	add_image_size( 'sponsor', 102, 52, true ); //(cropped)
+  register_post_type(
+    'sponsor',
+    array(
+      'label' => 'Sponsors',
+      'labels' => array(
+        'singular_name' => 'Sponsor',
+      ),
+      'description' => 'Een sponsor',
+      'public' => true,
+      'exclude_from_search' => true,
+      'publicly_queryable' => false,
+      'show_in_nav_menus' => false,
+      'menu_position' => 8,
+    )
+  );
+  add_image_size( 'sponsor', 102, 52, true ); //(cropped)
 
-	register_post_type(
-		'team',
-		array(
-			'label' => 'Teams',
-			'labels' => array(
-				'singular_name' => 'Team',
-			),
-			'description' => 'Een team',
-			'public' => true,
-			//'exclude_from_search' => false,
-			//'publicly_queryable' => false,
-			//'show_in_nav_menus' => true,
-			'menu_position' => 9,
-			'supports' => array(
-			  'title', 'editor', 'custom-fields', 'thumbnail'
-			),
-			'rewrite' => array(
-			  'slug' => 'team'
-			)
-		)
-	);
+  register_post_type(
+    'team',
+    array(
+      'label' => 'Teams',
+      'labels' => array(
+        'singular_name' => 'Team',
+      ),
+      'description' => 'Een team',
+      'public' => true,
+      //'exclude_from_search' => false,
+      //'publicly_queryable' => false,
+      //'show_in_nav_menus' => true,
+      'menu_position' => 9,
+      'supports' => array(
+        'title', 'editor', 'custom-fields', 'thumbnail'
+      ),
+      'rewrite' => array(
+        'slug' => 'team'
+      )
+    )
+  );
 
   //flush_rewrite_rules();
 
--- single.php
+++ single.php
@@ -16,62 +16,54 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 if ((get_post_type() == 'team') && has_post_thumbnail() ) {
 ?>
 <section id="header">
-  	<?php the_post_thumbnail('large'); ?>
+    <?php the_post_thumbnail('large'); ?>
 </section>
 <?php } ?>
 <?php get_sidebar('left'); ?>
 
-<!-- Needs check if header image exist, else: .no-header -->
-<?php
-if (!has_post_thumbnail()) {
-    $main_class='class="no-header"';
-} else {
-    $main_class="";
-}
-?>
-<section id="main" <?php echo $main_class; ?>>
+<section id="main" class="no-header">
   <div class="container white">
     <div class="row">
       <?php
     if (get_post_type() == 'team') {
-		  $table_width = 8;
+      $table_width = 8;
     } else {
-		  $table_width = 9;
+      $table_width = 9;
     }
       ?>
       <div id="nieuws" class="col-sm-8 col-md-<?php echo $table_width; ?>">
-				<?php
-				while (have_posts()) {
-					the_post();
+        <?php
+        while (have_posts()) {
+          the_post();
 
           if (get_post_type() == 'team') {
-					  get_template_part('content', 'team');
+            get_template_part('content', 'team');
           } else {
-					  get_template_part('content', get_post_format());
+            get_template_part('content', get_post_format());
           }
 
-					echo "\n\n";
+          echo "\n\n";
 
-					bootstrapBasicPagination();
+          bootstrapBasicPagination();
 
-					echo "\n\n";
+          echo "\n\n";
 
-					// If comments are open or we have at least one comment, load up the comment template
-					if (comments_open() || '0' != get_comments_number()) {
-						comments_template();
-					}
+          // If comments are open or we have at least one comment, load up the comment template
+          if (comments_open() || '0' != get_comments_number()) {
+            comments_template();
+          }
 
-					echo "\n\n";
+          echo "\n\n";
 
-				} //endwhile;
-				?>
+        } //endwhile;
+        ?>
       </div>
       <!-- sidebar thingie -->
       <?php
       if (get_post_type() == 'team') {
-			  get_template_part('content-sidebar', 'team');
+        get_template_part('content-sidebar', 'team');
       } else {
-			  get_template_part('content-sidebar', 'recent');
+        get_template_part('content-sidebar', 'recent');
       }
       ?>
     </div>
