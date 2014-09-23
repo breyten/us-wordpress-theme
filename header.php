fb9b490ede1baf0b6c9d88656de87cd03b49d826
Breyten Ernsting <bje@dds.nl>
2014-09-23 15:03:55 +0200

    Restore link to correct favicon.

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title><?php wp_title('|', true, 'right'); ?></title>
-    <link rel="icon" type="image/ico" href="/img/favicon.ico" />
+    <link rel="icon" type="image/ico" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 	<!--wordpress head-->
