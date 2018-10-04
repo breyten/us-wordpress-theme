62f333b47584746ba3fbed1827f735aeb9a4873b
Breyten Ernsting <bje@dds.nl>
2018-10-04 14:32:32 +0200

    Close the tag.

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
  */
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>
+<html <?php language_attributes(); ?>>
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
